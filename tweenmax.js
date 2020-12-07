f3a1d72bb3fa62bbad6703dd7dd37307990b289e
sexfat <bryant6831@gmail.com>
2020-12-07 15:35:54 +0800

    add

--- tweenmax.js
+++ tweenmax.js
@@ -2,13 +2,40 @@
 TweenMax.to('.box1', 2, {
     x: 600,
     // width: '100%',
-    y: 600,
-    repeat: 1, 
-    delay : 1,
+    //y: 600,
+    // repeat: -1, 
+    yoyo: true,
+    //delay : 1,
     repeatDelay: 1,
-    rotation : 360,
-    scale : 3,
-    opacity : 1, 
-    ease: Elastic.easeOut //補間
+    // rotation : 360,
+    // scale : 3,
+    opacity : 0,
+    backgroundColor : '#fff', 
+    //ease: Elastic.easeOut //補間
     // ease: Bounce.easeOut
 });
+
+var tl  = new TimelineMax({
+  repeat: -1,
+  yoyo: true
+
+});
+
+tl.to('.box2' , 1 , {
+    x: 600
+}).to('.box2' ,1 , {
+    y: 200
+}).to('.box2',1 , {
+    x: 400
+}).to('.box3' , 1 , {
+    rotation : 360
+})
+
+
+
+
+
+
+
+
+

52479a3297c0527d70a43b656b4f906d617959b4
sexfat <bryant6831@gmail.com>
2020-12-07 15:08:48 +0800

    asdd

--- tweenmax.js
+++ tweenmax.js
@@ -1,10 +1,14 @@
 //物件選取 秒數 位移
 TweenMax.to('.box1', 2, {
-    //x: 600,
-    width: '100%',
-    //y: 600,
-    repeat: -1, // 
+    x: 600,
+    // width: '100%',
+    y: 600,
+    repeat: 1, 
     delay : 1,
     repeatDelay: 1,
-    rotation : 360
+    rotation : 360,
+    scale : 3,
+    opacity : 1, 
+    ease: Elastic.easeOut //補間
+    // ease: Bounce.easeOut
 });

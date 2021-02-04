36ff9930ab0de8e1fbddea505a5c62ca081449ba
sexfat <bryant6831@gmail.com>
2021-02-04 09:49:41 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,34 @@ const {
     watch
 } = require('gulp');
 
-
-exports.task01 = function func01(cb){
+//輸出任務一
+function func01(cb){
     console.log('任務一');
     cb();
 }
+
+// exports.task01 = function func01(cb){
+//     console.log('任務一');
+//     cb();
+//  }
+
+//輸出任務二
+function func02(cb){
+    console.log('任務二');
+    cb();
+}
+
+//輸出任務三
+function func03(cb){
+    console.log('任務三');
+    cb();
+}
+
+exports.order = series(func01 ,func02); //順序執行任務
+exports.paral = parallel(func01 ,func02); //並行任務
+
+exports.all = series(func01 , parallel(func02 ,func03) );
+
 
 
 

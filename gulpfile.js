38a277affd3b402c8a82de1844589cfa238e1612
sexfat <bryant6831@gmail.com>
2021-02-04 09:30:16 +0800

    add 任務一

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,32 @@ const {
 } = require('gulp');
 
 
-const fileinclude = require('gulp-file-include');
- 
-exports.html =  function includeHTML(done) {
-    return src('*.html')
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(dest('./dist'));
-    done();
+exports.task01 = function func01(cb){
+    console.log('任務一');
+    cb();
 }
+
+
+
+
+
+
+
+// const fileinclude = require('gulp-file-include');
+
+
+
+
+
+
+
+
+// exports.html =  function includeHTML(done) {
+//     return src('*.html')
+//         .pipe(fileinclude({
+//             prefix: '@@',
+//             basepath: '@file'
+//         }))
+//         .pipe(dest('./dist'));
+//     done();
+// }

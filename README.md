cf895749543f750c14460b5b12cea62e46c89cb8
sexfat <bryant6831@gmail.com>
2021-02-18 10:06:03 +0800

    add webapck jquery

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 講義位址
 
 https://hackmd.io/chvUZNWERLO0uD9bBoQKXw?view
+
+# 環境的安裝
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('打包webpack');
+//套件引入
+import $ from "jquery";
+
+// jquery
+$('body').css('background' , '#f20');
+

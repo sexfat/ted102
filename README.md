9740ea1dbba586bf18700d43c2d6c8df8ec76af6
sexfat <bryant6831@gmail.com>
2021-02-18 10:21:17 +0800

    add gsap

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 https://hackmd.io/chvUZNWERLO0uD9bBoQKXw?view
 
-# 環境的安裝
+# 指令
+
+- webpack index.js -p 上線 
+- webpack index.js -d 開發
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 console.log('打包webpack');
 //套件引入
 import $ from "jquery";
+import { TweenMax } from "gsap";
 
 // jquery
-$('body').css('background' , '#f20');
+$('body').css('background' , '#f25');
+
+
+//gsap
+TweenMax.to('.box' , 1 ,{
+    x: 500,
+    y : 100,
+    rotation : 360
+})
+
+
 
